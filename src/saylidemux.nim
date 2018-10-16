b77ac4482d74c6688df134641e3cf8cc1a1617f2
Andreas WILM -CSB5 <wilma@gis.a-star.edu.sg>
2018-10-16 14:23:07 +0800

    Fixed strange error that resulted in empty barcode names and hence files with missing suffix

--- src/saylidemux.nim
+++ src/saylidemux.nim
@@ -83,21 +83,30 @@ proc main(fastq1: string, fastq2: string, barcodeYaml: string, outpref: string) 
   for k in keys bcMap:
     validBarcodes.add(k)
 
+  echo "DEBUG ", bcMap;
+
   for r1, r2 in parsePairedEnd(fastq1, fastq2):
     var bc1 = r1.sequence[0..5]
     var bc2 = r2.sequence[r2.len-6..<r2.len]
     var class = ""
-
+    var bc = ""
     bc1 = complement(reversed(bc1), DNA)
 
     # classify barcodes 
     if bc1 in validBarcodes and bc2 in validBarcodes:
       if bc1 == bc2:
         class = "both-valid"
+        bc = bc1
       else:
         class = "both-valid-but-different"
     elif bc1 in validBarcodes or bc2 in validBarcodes:
       class = "one-valid"
+      if bc1 in validBarcodes:
+        bc = bc1
+      elif bc2 in validBarcodes:
+        bc = bc2
+      else:
+       raise newException(ValueError, "Internal error")
     elif "N" in bc1 or "N" in bc2:
       class = "N-in-either"
     else:
@@ -109,13 +118,8 @@ proc main(fastq1: string, fastq2: string, barcodeYaml: string, outpref: string) 
     # write only the ones for which we have exactly one or two matches
     if class == "both-valid" or class == "one-valid":
       var bcName: string
-      try:
-        bcName = bcMap[bc1]
-      except KeyError:
-        let bcName = bcMap[bc2]
-      except:
-        raise
-
+      bcName = bcMap[bc]
+      assert bcName != ""
       if bcCounts.hasKey(bcName) == false:
         bcCounts[bcName] = 0
       bcCounts.inc(bcName)
